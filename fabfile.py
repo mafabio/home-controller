b48ad211b24903136c539819f131f5a6520dc8a0
Fabio Mauri <fabio.mauri@gmail.com>
2015-07-20 15:21:45 +0200

    Fixes to fabric file and init.d service launcher
    - fabric file fixed to match remote raspberry deploy
    - service launcher fixed to launch python interpreter
      and to manage auto-start
    
    Signed-off-by: Fabio Mauri <fabio.mauri@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,10 @@
 from fabric.api import *
 from fabric.contrib.project import rsync_project
-import os
-from time import strftime
 
-env.use_ssh_config = True
 env.user = 'root'
 
-env.roledefs = { 'test': ['192.168.101.146'], }
+env.roledefs = { 'raspberry': ['192.168.1.80'], }
 
-@roles('test')
+@roles('raspberry')
 def deploy():
   rsync_project(remote_dir='/opt/home-controller/', local_dir='./')
